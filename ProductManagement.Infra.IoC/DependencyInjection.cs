90fe3fc3121a9a1723c35c55de5fb86a07ce6a82
KevinGuedes <kevinguedes@edu.unifor.br>
2023-01-13 14:41:53 -0300

    first commit

---
+++ ProductManagement.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagement.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var applicationLayerAssembly = Assembly.Load("ProductManagement.Application");
+        }
+
+        private static void RegisterValidators(IServiceCollection services, Assembly applicationLayerAssembly)
+           => services.AddAutoMapper(applicationLayerAssembly);
+    }
+}

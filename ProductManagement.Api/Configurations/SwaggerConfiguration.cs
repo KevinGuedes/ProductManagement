95b4f1b8aafa3e46c78dda8429851b36a9ebabb7
KevinGuedes <kevinguedes@edu.unifor.br>
2023-01-14 11:30:17 -0300

    feat: configuration for swagger documentation

---
+++ ProductManagement.Api/Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace MyFinance.Presentation.Configurations
+{
+    public static class SwaggerConfiguration
+    {
+        public static void AddCustomSwaggerConfiguration(this IServiceCollection services)
+        {
+            services
+                .AddSwaggerGen(configuration =>
+                {
+                    configuration.EnableAnnotations();
+                    configuration.SwaggerDoc("v1", new OpenApiInfo
+                    {
+                        Version = "v1",
+                        Title = "ProductManagement API",
+                        Description = "API for Product Management",
+                        License = new OpenApiLicense { Name = "MIT" },
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Kevin Santos Gudes",
+                            Email = "kevinguedes1@gmail.com",
+                            Url = new Uri("https://www.linkedin.com/in/kevinsantosguedes/")
+                        }
+                    });
+                });
+        }
+
+        public static void UseCustomSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app
+                .UseSwagger()
+                .UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "ProductManagement API");
+                    options.DocumentTitle = "ProductManagement API";
+                });
+        }
+    }
+}
--- ProductManagement.Api/Startup.cs
+++ ProductManagement.Api/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using MyFinance.Presentation.Configurations;
 using ProductManagement.Infra.IoC;
 
 namespace ProductManagement.Api
@@ -20,15 +21,12 @@ namespace ProductManagement.Api
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCustomSwaggerConfiguration();
             services.InjectDependencies(Configuration);
+
             services
                 .AddControllers()
                 .AddOData(options => options.Select().Filter().OrderBy().Expand().Count().SetMaxTop(30));
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProductManagement.Api", Version = "v1" });
-            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -36,16 +34,12 @@ namespace ProductManagement.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProductManagement.Api v1"));
+                app.UseCustomSwaggerConfiguration();
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
